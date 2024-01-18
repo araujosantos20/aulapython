281af391db4cca71b5dbdfe919c28fd20c534b82
Senac Tatuape <senac@localhost.localdomain>
2024-01-18 11:51:45 -0300

    Funções

---
+++ Revisao/funcao1.py
@@ -0,0 +1,13 @@
+# Este programa possui uma função que diz quantas letras tem um texto
+
+# Para criar uma função é necessário utilizar o comando def
+
+def qtd_letras(texto):
+    print(len(texto))
+    """
+    A função qtletras recebe um texto e exibi
+    a quanidae de letras presentes no texto
+    """
+
+frase = "Hoje é quinta-feira"
+qtd_letras(frase)
---
+++ Revisao/funcao3.py
@@ -0,0 +1,7 @@
+# import funcao2 as fn
+from funcao2 import soma as sm
+
+n = [2, 16, 19, 26, 46 ,13, 53, 25]
+print(sm(n))
+# print(fn.soma(n))
+# print(fn.maior(n))
